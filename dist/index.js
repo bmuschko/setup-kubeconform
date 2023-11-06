0a8f462f9314cbb75c86efec8655e27e682cd6b6
Benjamin Muschko <benjamin.muschko@gmail.com>
2023-11-06 12:31:25 -0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+import * as util from 'util'
+
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+async function setup() {
+  // Get version of tool to be installed
+  const version = core.getInput('kubeconform_version');
+
+  // Download the specific version of the tool, e.g. as a tarball
+  const pathToTarball = await tc.downloadTool(getDownloadURL(version));
+
+  // Extract the tarball onto the runner
+  const pathToCLI = await tc.extractTar(pathToTarball);
+
+  // Expose the tool by adding it to the PATH
+  core.addPath(pathToCLI)
+}
+
+function getDownloadURL(version: string): string {
+  return util.format('https://github.com/yannh/kubeconform/releases/download/v%s/kubeconform-linux-amd64.tar.gz', version)
+}
+
+module.exports = setup
