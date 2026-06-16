f67ae77a9080b58c8565c2f568e00c38ef97dd77
Grigas Šukys <grigas.sukys@teliacompany.com>
2026-06-16 16:54:05 +0300

    test: update kubeconform version in tests
    - Updated kubeconform version in tests from "0.6.4" to "0.8.0".
    - Updated expected download URLs in tests for all platforms and architectures.

--- test/distribution.test.ts
+++ test/distribution.test.ts
@@ -1,7 +1,7 @@
 import * as distribution from '../src/distribution'
 
 describe('creates kubeconform distribution', () => {
-    const version = '0.6.4'
+    const version = '0.8.0'
 
     describe('for Linux', () => {
         const os = 'linux'
@@ -9,25 +9,25 @@ describe('creates kubeconform distribution', () => {
         it('with arch x32', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'x32')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-linux-386.tar.gz'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-linux-386.tar.gz'
             )
         })
         it('with arch x64', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'x64')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-linux-amd64.tar.gz'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-linux-amd64.tar.gz'
             )
         })
         it('with arch arm64', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'arm64')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-linux-arm64.tar.gz'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-linux-arm64.tar.gz'
             )
         })
         it('with arch armv6', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'armv6')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-linux-armv6.tar.gz'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-linux-armv6.tar.gz'
             )
         })
     })
@@ -38,13 +38,13 @@ describe('creates kubeconform distribution', () => {
         it('with arch x64', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'x64')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-darwin-amd64.tar.gz'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-darwin-amd64.tar.gz'
             )
         })
         it('with arch arm64', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'arm64')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-darwin-arm64.tar.gz'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-darwin-arm64.tar.gz'
             )
         })
     })
@@ -55,19 +55,19 @@ describe('creates kubeconform distribution', () => {
         it('with arch x32', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'x32')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-windows-386.zip'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-windows-386.zip'
             )
         })
         it('with arch x64', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'x64')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-windows-amd64.zip'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-windows-amd64.zip'
             )
         })
         it('with arch arm64', async () => {
             const dist = new distribution.KubeconformDistribution(version, os, 'arm64')
             expect(dist.getDownloadURL()).toBe(
-                'https://github.com/yannh/kubeconform/releases/download/v0.6.4/kubeconform-windows-arm64.zip'
+                'https://github.com/yannh/kubeconform/releases/download/v0.8.0/kubeconform-windows-arm64.zip'
             )
         })
     })
